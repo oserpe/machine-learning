2f4fd1597c727821a590f5e9bec37fb0c0c9cf81
manurodriguez <manurodriguez@itba.edu.ar>
2022-09-24 10:55:58 -0300

    :sparkles: break tie on KNN classify

--- ej2/main.py
+++ ej2/main.py
@@ -6,14 +6,19 @@ from .models.KNN import KNN
 def main(dataset):
     class_column = "Star Rating"
 
-    knn = KNN(dataset, class_column, 15)
     other = dataset.iloc[[0]]
     other = other.drop([class_column], axis=1)
     for i in [0,50,100,150]:
         sample = dataset.iloc[[i]]
         dataset.drop([i], inplace=True)
+        knn = KNN(dataset, class_column, 2)
+
         sample_without_class = sample.drop([class_column], axis=1)
-        print(knn.test(sample_without_class))
+        print("sample: ")
+        print(sample_without_class)
+        print("rating expected: ",knn.test(sample_without_class))
+
+        # restore dataset
         dataset = pd.concat([dataset,sample])
     return
 
@@ -21,7 +26,7 @@ if __name__ == "__main__":
     data_df = pd.read_csv(
         "./machine-learning/ej2/dataset/reviews_sentiment.csv", header=0, sep=';')
 
-    data_df.dropna(inplace=True) # TODO: CAMBIAR CRITERIO?
+    data_df.dropna(inplace=True) # TODO: CAMBIAR CRITERIO PARA REVIEWS CON DATOS FALTANTES?
 
     data_df.loc[data_df["titleSentiment"] == "positive", "titleSentiment"] = 1
     data_df.loc[data_df["titleSentiment"] == "negative", "titleSentiment"] = -1
@@ -31,7 +36,5 @@ if __name__ == "__main__":
     # drop text columns from df
     text_columns = ["Review Title", "Review Text"]
     numerical_data_df = data_df.drop(text_columns, axis=1)
-
-
 
     main(numerical_data_df)
--- ej2/models/KNN.py
+++ ej2/models/KNN.py
@@ -4,10 +4,10 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
 class KNN:
-    def __init__(self, train_dataset: pd.DataFrame, class_column: str, k: int):
+    def __init__(self, train_dataset: pd.DataFrame, class_column: str, start_k: int):
         self.train_dataset = train_dataset
         self.class_column = class_column
-        self.k = k
+        self.start_k = start_k
 
     def get_euclidean_distance(self, source: pd.DataFrame, dest: pd.DataFrame) -> float:
         return np.linalg.norm(source.values - dest.values, axis=1)
@@ -26,12 +26,28 @@ class KNN:
         std_sample_df = std_train_dataset_without_class.tail(1)
         std_train_dataset_without_class = std_train_dataset_without_class.drop(std_sample_df.index)
 
-        distances = []
-        for i in range(len(std_train_dataset_without_class)):
-            distances.append((self.get_euclidean_distance(std_train_dataset_without_class.iloc[[i]], std_sample_df), std_train_dataset_without_class.iloc[[i]].index[0]))
+        k = self.start_k
+        class_found = False
+        while k < len(std_train_dataset_without_class) and not class_found:
+            distances = []
+            for i in range(len(std_train_dataset_without_class)):
+                distances.append((
+                    self.get_euclidean_distance(std_train_dataset_without_class.iloc[[i]], std_sample_df),
+                    std_train_dataset_without_class.iloc[[i]].index[0])
+                )   # (distance, id)
 
-        # Calculamos los k vecinos más cercanos
-        k_nearest_neighbors_indexes = list(map(lambda x: x[1], sorted(
-            distances, key=lambda distance: distance[0])[:self.k]))
+            k_nearest_neighbors_indexes = list(map(lambda x: x[1], sorted(
+                distances, key=lambda distance: distance[0])[:k]))
 
-        return self.train_dataset.loc[k_nearest_neighbors_indexes][self.class_column].value_counts()
+            # Frequency of the classes of the k nearest neighbors ordered from highest to lowest
+            classes_by_appearances_sorted = self.train_dataset.loc[k_nearest_neighbors_indexes][self.class_column].value_counts()
+
+            # On tie, increase k and try again
+            if len(classes_by_appearances_sorted) == 1 \
+                or classes_by_appearances_sorted.iloc[0] != classes_by_appearances_sorted.iloc[1]:
+                class_found = True
+            else:
+                k += 1
+
+
+        return classes_by_appearances_sorted.iloc[[0]].index[0]
