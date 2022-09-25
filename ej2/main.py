ef8e4ecdf87c1d6e83b73d8cc9355655f55f1b3a
Gonzalo Arca <goarca@itba.edu.ar>
2022-09-25 11:35:44 -0300

    :art: improve KNN code format and structure

--- ej2/main.py
+++ ej2/main.py
@@ -3,15 +3,18 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 from .models.KNN import KNN
 
+
 def main(dataset):
     class_column = "Star Rating"
+    X = dataset.drop(class_column, axis=1)
+    Y = dataset[[class_column]]
 
     other = dataset.iloc[[0]]
     other = other.drop([class_column], axis=1)
     for i in [0, 50, 100, 200]:
         sample = dataset.iloc[[i]]
         dataset.drop([i], inplace=True)
-        knn = KNN(dataset, class_column, 2, weighted=False)
+        knn = KNN(X, Y, 2, weighted=False)
 
         sample_without_class = sample.drop([class_column], axis=1)
         print("sample: ")
@@ -19,14 +22,16 @@ def main(dataset):
         print("rating expected: ", knn.classify(sample_without_class))
 
         # restore dataset
-        dataset = pd.concat([dataset,sample])
+        dataset = pd.concat([dataset, sample])
     return
+
 
 if __name__ == "__main__":
     data_df = pd.read_csv(
         "./machine-learning/ej2/dataset/reviews_sentiment.csv", header=0, sep=';')
 
-    data_df.dropna(inplace=True) # TODO: CAMBIAR CRITERIO PARA REVIEWS CON DATOS FALTANTES?
+    # TODO: CAMBIAR CRITERIO PARA REVIEWS CON DATOS FALTANTES?
+    data_df.dropna(inplace=True)
 
     data_df.loc[data_df["titleSentiment"] == "positive", "titleSentiment"] = 1
     data_df.loc[data_df["titleSentiment"] == "negative", "titleSentiment"] = -1
--- ej2/models/KNN.py
+++ ej2/models/KNN.py
@@ -3,55 +3,59 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
+
 class KNN:
-    def __init__(self, train_dataset: pd.DataFrame, class_column: str, start_k: int, weighted: bool):
-        self.train_dataset = train_dataset
-        self.class_column = class_column
-        self.start_k = start_k
+    def __init__(self, X: pd.DataFrame, Y: pd.DataFrame, k_neighbors: int, weighted: bool):
+        self.X = X
+        self.Y = Y
+        self.class_column = Y.columns[0]
+        self.k_neighbors = k_neighbors
         if weighted:
-            self.get_k_classes_sorted = self.get_K_classes_sorted_by_weight
+            self.get_ranked_classes = self.get_ranked_classes_by_weight
         else:
-            self.get_k_classes_sorted = self.get_K_classes_sorted_by_appearances
+            self.get_ranked_classes = self.get_ranked_classes_by_appearances
 
     def get_euclidean_distance(self, source: pd.DataFrame, dest: pd.DataFrame) -> float:
         return np.linalg.norm(source.values - dest.values, axis=1)
 
     def classify(self, sample):
-        std_train_dataset_without_class = self.train_dataset.drop([self.class_column], axis=1)
-
-        # add sample to dataset before standarizing
-        std_train_dataset_without_class = pd.concat([std_train_dataset_without_class, sample], axis = 0, sort = False)
+        # add sample to dataset before standardizing
+        std_X = pd.concat(
+            [self.X, sample], axis=0, sort=False)
 
         # standarize values that will be used by KNN
-        columns = std_train_dataset_without_class.columns
-        std_train_dataset_without_class[columns] = \
-                    StandardScaler().fit_transform(std_train_dataset_without_class[columns])
+        columns = std_X.columns
+        std_X[columns] = \
+            StandardScaler().fit_transform(
+                std_X[columns])
 
-        # Once it is standarized with the train data, get the std sample
-        std_sample_df = std_train_dataset_without_class.tail(1)
-        std_train_dataset_without_class = std_train_dataset_without_class.drop(std_sample_df.index)
+        # Once it is standarized with the train data, remove and retrieve the std sample
+        std_sample_df = std_X.tail(1)
+        std_X = std_X.drop(
+            std_sample_df.index)
 
-        k = self.start_k
+        k = self.k_neighbors
         class_found = False
-        while not class_found and k < len(std_train_dataset_without_class):
+        while not class_found and k < len(std_X):
             distances = []
-            for i in range(len(std_train_dataset_without_class)):
-                row = std_train_dataset_without_class.iloc[[i]]
+            for i in range(len(std_X)):
+                row = std_X.iloc[[i]]
                 distance = self.get_euclidean_distance(row, std_sample_df)
 
                 distances.append((
                     distance,
                     row.index[0]
-                    ))   # (distance, id)
+                ))   # (distance, id)
 
             k_nearest_neighbors_indexes = list(map(lambda x: x[1], sorted(
                 distances, key=lambda distance: distance[0])[:k]))
 
-            classes_by_appearances_sorted = self.get_k_classes_sorted(k_nearest_neighbors_indexes, k, distances)
+            classes_by_appearances_sorted = self.get_ranked_classes(
+                k_nearest_neighbors_indexes, k, distances)
 
             # On tie, increase k and try again
             if len(classes_by_appearances_sorted) == 1 \
-                or classes_by_appearances_sorted.iloc[0] != classes_by_appearances_sorted.iloc[1]:
+                    or classes_by_appearances_sorted.iloc[0] != classes_by_appearances_sorted.iloc[1]:
                 class_found = True
             else:
                 k += 1
@@ -61,27 +65,29 @@ class KNN:
 
         return classes_by_appearances_sorted.iloc[[0]].index[0]
 
-    def get_K_classes_sorted_by_weight(self, k_nearest_neighbors_indexes, k, distances):
-        zero_distance_neighbours = list(map(lambda x: x[1], 
-            list(filter(lambda x: x[0] == 0, distances))))
+    def get_ranked_classes_by_weight(self, k_nearest_neighbors_indexes, k, distances):
+        zero_distance_neighbours = list(map(lambda x: x[1],
+                                            list(filter(lambda x: x[0] == 0, distances))))
         if len(zero_distance_neighbours) > 0:
             # If there are neighbours with zero distance, return the class of the most popular between them
-            return self.get_K_classes_sorted_by_appearances(zero_distance_neighbours)
+            return self.get_ranked_classes_by_appearances(zero_distance_neighbours)
 
         # add inv_distance column before grouping
         k_nearest_neighbors_inverse_distances = list(map(lambda x: 1/(x[0])**2, sorted(
             distances, key=lambda distance: distance[0])[:k]))
-        k_nearest_neighbors = self.train_dataset.loc[k_nearest_neighbors_indexes]
+
+        # keep only the k nearest neighbours from the original dataset, with their class and inv_distance
+        k_nearest_neighbors = self.Y.loc[k_nearest_neighbors_indexes]
         k_nearest_neighbors["inv_distance"] = k_nearest_neighbors_inverse_distances
 
         classes_by_appearances_sorted = k_nearest_neighbors\
             .groupby(self.class_column)["inv_distance"].sum().sort_values(ascending=False)
-        
+
         return classes_by_appearances_sorted
 
-    def get_K_classes_sorted_by_appearances(self, k_nearest_neighbors_indexes, k, distances):
+    def get_ranked_classes_by_appearances(self, k_nearest_neighbors_indexes, k, distances):
         # Frequency of the classes of the k nearest neighbors ordered from highest to lowest without weight
-        classes_by_appearances_sorted = self.train_dataset.loc[k_nearest_neighbors_indexes]\
+        classes_by_appearances_sorted = self.Y.loc[k_nearest_neighbors_indexes]\
             .groupby(self.class_column).size().sort_values(ascending=False)
-        
+
         return classes_by_appearances_sorted
