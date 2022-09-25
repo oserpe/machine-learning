cf685662dfe9cae2a682889297a20b0c2ce1b9f1
Gonzalo Arca <goarca@itba.edu.ar>
2022-09-25 16:19:31 -0300

    :sparkles: add histogram plot for all variables and account balance plot
    Co-authored-by: Octavio Serpe <oserpe@itba.edu.ar>

--- ej1/data_analysis/data_analysis.py
+++ ej1/data_analysis/data_analysis.py
@@ -11,14 +11,38 @@ def plot_creditability_histogram(dataset: pd.DataFrame):
 
     # plot histogram
     display_data.apply(pd.value_counts).T.plot(kind='bar', rot=0)
-    plt.show(block=True)
+
+
+def plot_account_balance_histogram(dataset: pd.DataFrame):
+    # change 0 to "No" and 1 to "Yes"
+    display_data = dataset[["Account Balance"]].replace(
+        {1: "Sin cuenta", 2: "Poco balanceada", 3: "Balanceada", 4: "Bien balanceada"})
+
+    display_data["Creditability"] = dataset[["Creditability"]].replace(
+        {0: "No", 1: "Yes"})
+
+    display_data = display_data.groupby(
+        ["Account Balance", "Creditability"])["Account Balance"].count().unstack("Creditability")
+
+    display_data.plot(kind='bar', rot=0, stacked=True,
+                      title="Creditability dado Account Balance", ylabel="Cantidad de ejemplares")
+
+
+def plot_general_histogram(dataset: pd.DataFrame):
+    dataset.drop(["Creditability"], axis=1).hist(edgecolor='black', linewidth=1.0,
+                                                 xlabelsize=10, ylabelsize=10, grid=False)
+    plt.tight_layout()
 
 
 if __name__ == "__main__":
     data_df = pd.read_csv(
         "./machine-learning/ej1/dataset/german_credit.csv", header=0, sep=',')
 
-    plot_creditability_histogram(data_df)
+    plot_general_histogram(data_df)
+    plt.show(block=True)
+
+    # plot_account_balance_histogram(data_df)
+    # plot_creditability_histogram(data_df)
     # dataset[["Creditability"]].plot(
     #     kind='hist', bins=2, xticks=[0, 1])
     # plt.show(block=True)
