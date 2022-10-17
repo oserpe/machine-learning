f3b9a76c88e907dbcde7d6aaf14259033eb7e97c
manurodriguez <manurodriguez@itba.edu.ar>
2022-10-17 11:00:52 -0300

    :sparkles: add svm test of different kernels and C values

--- ej2/main.py
+++ ej2/main.py
@@ -1,8 +1,9 @@
 import numpy as np
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, test
 from PIL import Image
 from enum import Enum
-from sklearn.model_selection import train_test_split
+from sklearn import svm, model_selection, utils
+
 
 class ImageClasses(Enum):
     GRASS = 0
@@ -10,32 +11,74 @@ class ImageClasses(Enum):
     COW = 2
 
 
-def get_data_from_image(image_path, image_class):
+def get_data_from_images(images_data):
     X = []
     y = []
-    training_img = Image.open(image_path)
+    for image_path, image_class in images_data:
+        training_img = Image.open(image_path)
 
-    for pixel in training_img.getdata():
-        X.append(pixel)
-        y.append(image_class.value)
+        for pixel in training_img.getdata():
+            X.append(pixel)
+            y.append(image_class.value)
 
     return X, y
+
+
+def test_smv(X_train, X_test, y_train, y_test, kernel, C=1, gamma='scale'):
+    clf = svm.SVC(kernel=kernel, C=C, gamma=gamma)
+    clf.fit(X_train, y_train)
+    print(f"SVM {kernel} FITTED")
+
+    return clf.score(X_test, y_test)
+
+
+def test_different_smv_kernels(X_train, X_test, y_train, y_test):
+    kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+    
+    scores = []
+    for kernel in kernels:
+        print(f"Testing {kernel} kernel")
+        scores.append(test_smv(X_train, X_test, y_train, y_test, kernel))
+        print(f"Score for kernel {kernel}: {scores[-1]}")
+    
+    return scores
+
+
+def test_different_smv_C(X_train, X_test, y_train, y_test):
+    C_values = [0.1, 1, 10, 100, 1000]
+    
+    scores = []
+    for C in C_values:
+        print(f"Testing C={C}")
+        scores.append(test_smv(X_train, X_test, y_train, y_test, 'rbf', C=C))
+        print(f"Score for C={C}: {scores[-1]}")
+    
+    return scores
 
 
 if __name__ == "__main__":
     random_state = 1
     X = []
     y = []
-    X, y += get_data_from_image(
-        "ej2/data/partial_grass.jpg", ImageClasses.GRASS)
-    X, y += get_data_from_image(
-        "ej2/data/partial_sky.jpg", ImageClasses.SKY)
-    X, y += get_data_from_image(
-        "ej2/data/partial_cow.jpg", ImageClasses.COW)
+
+    images_directory = "machine-learning/ej2/data/"
+    images_data = [
+        (images_directory + "partial_grass.jpg", ImageClasses.GRASS),
+        (images_directory + "partial_sky.jpg", ImageClasses.SKY),
+        (images_directory + "partial_cow.jpg", ImageClasses.COW)]
+
+    X, y = get_data_from_images(images_data)
 
     # Shuffle the data
-    np.random.seed(random_state)
-    np.random.shuffle(X)
+    X, y = utils.shuffle(X, y, random_state=random_state)
 
     # Separate data into training and test data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=random_state)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.33, random_state=random_state)
+    print("READY TO FIT")
+
+    # Test different kernels
+    # test_different_smv_kernels(X_train, X_test, y_train, y_test)
+
+    # Test different C values
+    test_different_smv_C(X_train, X_test, y_train, y_test)
+
