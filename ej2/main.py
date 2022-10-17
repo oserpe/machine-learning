87f5d938c709d06f8059f72ef94b6a01be52b4f1
manurodriguez <manurodriguez@itba.edu.ar>
2022-10-17 10:03:52 -0300

    :card_file_box: read data and divide into train and test

--- ej2/main.py
+++ ej2/main.py
@@ -1,5 +1,41 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image
+from enum import Enum
+from sklearn.model_selection import train_test_split
+
+class ImageClasses(Enum):
+    GRASS = 0
+    SKY = 1
+    COW = 2
+
+
+def get_data_from_image(image_path, image_class):
+    X = []
+    y = []
+    training_img = Image.open(image_path)
+
+    for pixel in training_img.getdata():
+        X.append(pixel)
+        y.append(image_class.value)
+
+    return X, y
+
 
 if __name__ == "__main__":
-    seed = 2
+    random_state = 1
+    X = []
+    y = []
+    X, y += get_data_from_image(
+        "ej2/data/partial_grass.jpg", ImageClasses.GRASS)
+    X, y += get_data_from_image(
+        "ej2/data/partial_sky.jpg", ImageClasses.SKY)
+    X, y += get_data_from_image(
+        "ej2/data/partial_cow.jpg", ImageClasses.COW)
+
+    # Shuffle the data
+    np.random.seed(random_state)
+    np.random.shuffle(X)
+
+    # Separate data into training and test data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=random_state)
