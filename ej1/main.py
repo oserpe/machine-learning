:bug: wip: fix three point SVM margin calculation

ej1/main.py
@@ -1,5 +1,4 @@
 import math
-from turtle import left
 import numpy as np
 from sklearn.model_selection import GridSearchCV, train_test_split, cross_val_score
 import itertools
@@ -19,13 +18,15 @@ from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 
 # TODO: move to utils.py
+
+
 def generate_line_interval(m, b, interval):
     return [m * interval[0] + b, m * interval[1] + b]
 
 
-def plot_ej_a(X_train, y_train, X_test, y_test, m, b, interval, seed, animate=False):
+def plot_ej_a(X_train, y_train, X_test, y_test, interval, seed, animate=False):
     # Classify the points using the perceptron
-    perceptron = SimplePerceptron(eta=0.01, max_iter=1000, max_epochs=1000,
+    perceptron = SimplePerceptron(eta=0.01, max_iter=1200, max_epochs=1000,
                                   tol=0.01, random_state=seed, verbose=False)
 
     perceptron.fit(X_train, y_train)
@@ -38,15 +39,17 @@ def plot_ej_a(X_train, y_train, X_test, y_test, m, b, interval, seed, animate=Fa
         fig, ax = plt.subplots(1, 1)
 
         # Plot the hyperplane animation
-        animate_func = get_animation_function(perceptron, X_train, y_train, interval, ax, "Perceptron classification with linearly separable dataset")
+        animate_func = get_animation_function(
+            perceptron, X_train, y_train, interval, ax, "Perceptron classification with linearly separable dataset")
         anim = animation.FuncAnimation(fig, animate_func, frames=len(
             perceptron.w_list), repeat=False, interval=25)
-        
-        anim.save('./machine-learning/ej1/dump/linearly_dataset_perceptron_animation.gif', fps=2)
-        plt.show()   
+
+        anim.save(
+            './machine-learning/ej1/dump/linearly_dataset_perceptron_animation.mp4', fps=20)
+        plt.show()
     else:
         plot_data(X_train, y_train, interval, [[*perceptron.w_, perceptron.b_]],
-              title="Perceptron classification with linearly separable dataset", colors=['green'], labels=['Predicted']) 
+                  title="Perceptron classification with linearly separable dataset", colors=['green'], labels=['Predicted'])
 
     # PLOT: n k fold cross validation
     # plot_n_k_fold_cv_eval(X, y, 5, perceptron, k=5)
@@ -69,7 +72,7 @@ def plot_ej_a(X_train, y_train, X_test, y_test, m, b, interval, seed, animate=Fa
 
 def plot_ej_b(X_train, X_test, y_train, y_test, interval, seed):
     # Classify the points using the perceptron
-    perceptron = SimplePerceptron(eta=0.01, max_iter=10000, max_epochs=1000,
+    perceptron = SimplePerceptron(eta=0.01, max_iter=1200, max_epochs=1000,
                                   tol=0.01, random_state=seed, verbose=False)
 
     perceptron.fit(X_train, y_train)
@@ -79,24 +82,27 @@ def plot_ej_b(X_train, X_test, y_train, y_test, interval, seed):
     three_points_svm = ThreePointsSVM()
     three_points_svm.fit(X_train, y_train, [m, b])
 
-    plt.scatter(three_points_svm.support_points_x, three_points_svm.support_points_y, c="violet", s=170)
+    plt.scatter(three_points_svm.support_points_x,
+                three_points_svm.support_points_y, c="violet", s=170)
 
     # find accuracy
     y_pred = three_points_svm.predict(X_test)
     accuracy = np.sum(y_pred == y_test) / len(y_test)
     print("test accuracy: ", accuracy)
 
+    print("Optimal hyperplane confidence: ",
+          three_points_svm.confidence_margin)
+    print("Perceptron confidence: ", three_points_svm.perceptron_confidence)
+
     # TRAIN HYPERPLANE
     plot_data(X_train, y_train, interval, [three_points_svm.optimum_hyperplane_data, *three_points_svm.support_hyperplanes_data, [m, -1, b]],
-              title="Optimal hyperplane", colors=['green', 'blue', 'blue', 'red'], 
+              title="Optimal hyperplane", colors=['green', 'blue', 'blue', 'red'],
               styles=['-', '--', '--', '-'], with_support_hyperplanes=True,
               labels=['Optimal hyperplane', 'Support hyperplane', 'Support hyperplane', 'Perceptron'])
 
-    print("Optimal hyperplane confidence: ", three_points_svm.confidence_margin)
-    print("Perceptron confidence: ", three_points_svm.perceptron_confidence)
     # TEST
     # plot_data(X_test, y_test, interval, [three_points_svm.optimum_hyperplane_data, *three_points_svm.support_hyperplanes_data, [m, -1, b]],
-    #           title="Optimal hyperplane with test dataset", colors=['green', 'blue', 'blue', 'red'], 
+    #           title="Optimal hyperplane with test dataset", colors=['green', 'blue', 'blue', 'red'],
     #           styles=['-', '--', '--', '-'], with_support_hyperplanes=True,
     #           labels=['Optimal hyperplane', 'Support hyperplane', 'Support hyperplane', 'Perceptron'])
 
@@ -115,11 +121,13 @@ def plot_ej_c(X, y, interval, seed):
     fig, ax = plt.subplots(1, 1)
 
     # Plot the hyperplane animation
-    animate = get_animation_function(perceptron, X, y, interval, ax, "Perceptron classification with not linearly separable dataset")
+    animate = get_animation_function(
+        perceptron, X, y, interval, ax, "Perceptron classification with not linearly separable dataset")
     anim = animation.FuncAnimation(fig, animate, frames=len(
         perceptron.w_list), repeat=False, interval=0)
-    
-    anim.save('./machine-learning/ej1/dump/not_linearly_dataset_perceptron_animation.mp4', fps=5)
+
+    anim.save(
+        './machine-learning/ej1/dump/not_linearly_dataset_perceptron_animation.mp4', fps=20)
     plt.show()
 
 
@@ -220,18 +228,22 @@ if __name__ == "__main__":
 
     X, y, m, b = generate_linearly_separable(
         n, interval, seed, clustering=True, hyperplane_margin=0.5)
+
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=seed)
 
     # perceptron_grid_search_params = {
-    #     "eta": [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1]}
-    # execute_grid_search_cv(SimplePerceptron(eta=0.001, max_iter=1000, max_epochs=1000,
-    #                                         tol=0.01, random_state=seed, verbose=False), X_train, X_test,
-    #                        y_train, y_test, perceptron_grid_search_params)
+    #     "eta": [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1],
+    #     "max_iter": [300, 500, 700, 1000, 1200]}
 
-    plot_ej_a(X_train, y_train, X_test, y_test, m, b, interval, seed)
+    # execute_grid_search_cv(SimplePerceptron(eta=0.001, max_iter=1000, max_epochs=1000,
+    #                                         tol=0.01, random_state=seed, verbose=False), X, X_test,
+    #                        y, y_test, perceptron_grid_search_params)
+
+    # plot_ej_a(X_train, y_train, X_test, y_test, interval, seed, animate=True)
+    plot_ej_b(X_train, X_test, y_train, y_test, interval, seed)
+    # plot_ej_b(X_train, y_train, X_test, y_test, interval, seed)
     # plot_ej_b(X, X_test, y, y_test, interval, seed)
-    
 
     # noise_prox = 0.2
     # noise_prob = 0.5

models/ThreePointsSVM.py
@@ -15,9 +15,9 @@ class ThreePointsSVM(BaseEstimator):
         m = initial_hyperplane[0]
         b = initial_hyperplane[1]
 
-        for x_i, y_i in zip(X,y):
+        for x_i, y_i in zip(X, y):
             distance = abs((m * x_i[0] - x_i[1] + b) / np.sqrt(m ** 2 + 1))
-            
+
             point_with_distance = (distance, x_i)
             if y_i == 1:
                 points_distance_positive_class.append(point_with_distance)
@@ -29,8 +29,9 @@ class ThreePointsSVM(BaseEstimator):
         points_distance_positive_class.sort(key=lambda x: x[0])
 
         # Save the perceptron confidence for study purposes
-        self.perceptron_confidence = min(points_distance_positive_class[0][0], points_distance_negative_class[0][0])
-        
+        self.perceptron_confidence = min(
+            points_distance_positive_class[0][0], points_distance_negative_class[0][0])
+
         # Find the hyperplane with the biggest margin
         self.confidence_margin = 0
 
@@ -44,34 +45,50 @@ class ThreePointsSVM(BaseEstimator):
                 one_point_class = points_distance_negative_class
 
             # Get the two closest points of the two_points_class and calculate first support hyperplane
-            line_support_point1, line_support_point2 = list(map(lambda x: x[1], two_points_class[:2]))
-            new_m = (line_support_point1[1] - line_support_point2[1]) / (line_support_point1[0] - line_support_point2[0])
+            line_support_point1, line_support_point2 = list(
+                map(lambda x: x[1], two_points_class[:2]))
+            new_m = (line_support_point1[1] - line_support_point2[1]) / \
+                (line_support_point1[0] - line_support_point2[0])
             new_b = line_support_point1[1] - new_m * line_support_point1[0]
 
             first_support_hyp = [new_m, -1, new_b]
-            
+
             # Get the closest point of the other class and calculate second support hyperplane using the previous one
             other_support_point = one_point_class[0][1]
-            other_support_hyp = [new_m, -1, other_support_point[1] - new_m * other_support_point[0]]
-            other_hyp_b = other_support_point[1] - new_m * other_support_point[0]
+            other_support_hyp = [
+                new_m, -1, other_support_point[1] - new_m * other_support_point[0]]
+            other_hyp_b = other_support_point[1] - \
+                new_m * other_support_point[0]
             # Find the optimum hyperplane using previous support hyperplanes
             optimum_hyperplane = [new_m, -1, (new_b + other_hyp_b) / 2]
 
             # Find if this hyperplane margin is better than the previous one, if so, save its data
-            new_confidence_margin = abs(new_b - other_hyp_b) / 2
+            x = [points_distance_positive_class[0],
+                 points_distance_negative_class[0]].sort(key=lambda x: x[0])[1]
+            new_confidence_margin = self._calculate_distance_to_hyperplane(
+                x, optimum_hyperplane)
             if new_confidence_margin > self.confidence_margin:
                 self.confidence_margin = new_confidence_margin
-                self.support_points_x = [line_support_point1[0], line_support_point2[0], other_support_point[0]]
-                self.support_points_y = [line_support_point1[1], line_support_point2[1], other_support_point[1]]
+                self.support_points_x = [
+                    line_support_point1[0], line_support_point2[0], other_support_point[0]]
+                self.support_points_y = [
+                    line_support_point1[1], line_support_point2[1], other_support_point[1]]
                 self.optimum_hyperplane_data = optimum_hyperplane
-                self.support_hyperplanes_data = [first_support_hyp, other_support_hyp]
+                self.support_hyperplanes_data = [
+                    first_support_hyp, other_support_hyp]
 
+    def _calculate_distance_to_hyperplane(self, x, hyperplane):
+        m = hyperplane[0]
+        b = hyperplane[2]
+
+        return abs((m * x[0] - x[1] + b) / np.sqrt(m ** 2 + 1))
 
     def predict(self, X):
         predictions = []
         for x_i, y_i in X:
             # Find if x_i is above or below the hyperplane
-            line_y_at_x_i = self.optimum_hyperplane_data[0] * x_i + self.optimum_hyperplane_data[2]
+            line_y_at_x_i = self.optimum_hyperplane_data[0] * \
+                x_i + self.optimum_hyperplane_data[2]
             predictions.append(-1*np.sign(line_y_at_x_i - y_i))
 
         return predictions