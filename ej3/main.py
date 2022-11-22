efeb3113b6d0c55945d429b1461d5c863771ff81
Gonzalo Arca <goarca@itba.edu.ar>
2022-11-22 16:00:27 -0300

    :bug: fix pandas bugs in classifier model

--- ej3/main.py
+++ ej3/main.py
@@ -1,6 +1,44 @@
 import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from ..models.unsupervised_classifier import UnsupervisedClassifier
+
+
+def find_hyperparameters_kmeans(k_range, max_iter_range, random_state_range, X_train, y_train, X_test, y_test):
+    grid = GridSearchCV(UnsupervisedClassifier(model="kmeans", K=1, max_iter=100, random_state=0, verbose=False), param_grid={
+        'K': k_range,
+        'max_iter': max_iter_range,
+        'random_state': random_state_range,
+        'model': ['kmeans']
+    }, cv=5, n_jobs=-1, verbose=3)
+
+    # fitting the model for grid search
+    grid.fit(X_train, y_train)
+
+    # print best parameter after tuning
+    print(grid.best_params_)
+    grid_predictions = grid.predict(X_test)
+    print(classification_report(y_test, grid_predictions))
+
+
+def find_hyperparameters_kohonen(X_train, y_train, X_test, y_test):
+    grid = GridSearchCV(UnsupervisedClassifier(model="kohonen", K=1, max_iter=100, random_state=0, verbose=False), param_grid={
+        'K': [3, 4, 5, 6],
+        'max_iter': [100, 200, 300, 400, 500],
+        'random_state': [0, 1, 2, 3, 4, 5],
+        'kohonen_initial_lr': [0.01, 0.1, 0.5, 1],
+        'kohonen_initial_radius': [1, 2, 3, 4, 5],
+        'model': ['kohonen']
+    }, cv=5, n_jobs=-1, verbose=3)
+
+    # fitting the model for grid search
+    grid.fit(X_train, y_train)
+
+    # print best parameter after tuning
+    print(grid.best_params_)
+    grid_predictions = grid.predict(X_test)
+    print(classification_report(y_test, grid_predictions))
 
 
 if __name__ == "__main__":
@@ -24,9 +62,9 @@ if __name__ == "__main__":
     GENRES_TO_ANALYZE = ["Adventure", "Comedy", "Drama"]
     movies_df = movies_df[movies_df["genres"].isin(GENRES_TO_ANALYZE)]
 
-    movies_df = movies_df.sample(frac=0.05, random_state=random_state)
+    movies_df = movies_df.sample(frac=0.1, random_state=random_state)
 
-    only_genres_df = movies_df["genres"]
+    only_genres_df = movies_df[["genres"]]
     # once removed not interesting genres, we remove the column for the grouping process over numerical variables
     movies_df = movies_df.drop(columns=["genres"])
 
@@ -35,12 +73,18 @@ if __name__ == "__main__":
         movies_df), columns=movies_df.columns)
 
     X_features = movies_df.columns.tolist()
-    y_feature = only_genres_df.name
+    y_feature = only_genres_df.columns.tolist()
 
-    unsupervised_classifier = UnsupervisedClassifier(
-        "hierarchical", K=10, max_iter=100, random_state=random_state)
-    unsupervised_classifier.fit(
-        movies_df.values, only_genres_df.values, X_features, y_feature)
-    predictions = unsupervised_classifier.predict(movies_df.values[:5])
+    find_hyperparameters_kmeans(k_range=range(3, 5), max_iter_range=range(100, 1000, 200), random_state_range=range(
+        0, 5), X_train=movies_df[X_features], y_train=only_genres_df, X_test=movies_df[X_features], y_test=only_genres_df)
 
-    print("predictions: ", predictions)
+    # unsupervised_classifier = UnsupervisedClassifier(
+    #     "hierarchical", K=10, max_iter=100, random_state=random_state)
+    # unsupervised_classifier.fit(
+    #     movies_df[X_features], only_genres_df)
+
+    # print(unsupervised_classifier.score(
+    #     movies_df[X_features], only_genres_df))
+    # predictions = unsupervised_classifier.predict(movies_df.values[:5])
+
+    # print("predictions: ", predictions)
--- models/unsupervised_classifier.py
+++ models/unsupervised_classifier.py
@@ -4,57 +4,72 @@ import numpy as np
 import pandas as pd
 from .cluster import ClusteringDistance
 from .decay_functions import exp_decay
+from sklearn.base import BaseEstimator
 
 
-class UnsupervisedClassifier():
+class UnsupervisedClassifier(BaseEstimator):
     def __init__(self, model, K, max_iter, random_state, kohonen_initial_lr=0.01,
                  kohonen_initial_radius=None, kohonen_lr_fn=exp_decay, kohonen_radius_fn=exp_decay,
                  hierarchical_distance_metric=ClusteringDistance.CENTROID, verbose=True):
+        self.K = K
+        self.max_iter = max_iter
+        self.random_state = random_state
+        self.kohonen_initial_lr = kohonen_initial_lr
+        self.kohonen_initial_radius = kohonen_initial_radius
+        self.kohonen_lr_fn = kohonen_lr_fn
+        self.kohonen_radius_fn = kohonen_radius_fn
+        self.hierarchical_distance_metric = hierarchical_distance_metric
+        self.verbose = verbose
+        self.model = model
+
         if model == 'kmeans':
             from .k_means import KMeans
-            self.model = KMeans(K=K, max_iter=max_iter,
-                                random_state=random_state, verbose=verbose)
+            self._model = KMeans(K=K, max_iter=max_iter,
+                                 random_state=random_state, verbose=verbose)
         elif model == 'hierarchical':
             from .hierarchical_clustering import HierarchicalClustering
-            self.model = HierarchicalClustering(
+            self._model = HierarchicalClustering(
                 K=K, verbose=verbose, distance_metric=hierarchical_distance_metric)
         elif model == 'kohonen':
             from .kohonen import Kohonen
             if kohonen_initial_radius is None:
                 kohonen_initial_radius = K
-            self.model = Kohonen(K=K, max_iter=max_iter, random_state=random_state, verbose=verbose, initial_lr=kohonen_initial_lr,
-                                 initial_radius=kohonen_initial_radius, lr_fn=kohonen_lr_fn, radius_fn=kohonen_radius_fn)
+            self._model = Kohonen(K=K, max_iter=max_iter, random_state=random_state, verbose=verbose, initial_lr=kohonen_initial_lr,
+                                  initial_radius=kohonen_initial_radius, lr_fn=kohonen_lr_fn, radius_fn=kohonen_radius_fn)
         else:
             raise ValueError('Invalid model')
 
     def get_feature_index(self, feature):
         return self.features.index(feature)
 
-    def fit(self, X, y, X_features, y_feature):
-        self.model.fit(X)
-        self.X_features = X_features
-        self.y_feature = y_feature
+    def fit(self, X, y):
+        self.X_features = X.columns
+        self.y_feature = y.columns[0]
+
+        self._model.fit(X.to_numpy())
 
         # Create dataframe from X and y
-        full_data = np.concatenate((X, y.reshape(-1, 1)), axis=1)
-        df = pd.DataFrame(full_data, columns=[*X_features, y_feature])
+
+        df = pd.concat([X.reset_index(), y.reset_index()], axis=1)
+
         # Change from object to float64 the X_features columns
-        df = df.astype({feature: 'float64' for feature in X_features})
+        df = df.astype({feature: 'float64' for feature in self.X_features})
         self.clusters = []
 
         # For each cluster, get their points and add the remaining features (y's)
         # iteration = -1
-        for cluster in self.model.get_clusters():
-            df_points = pd.DataFrame(cluster.points, columns=X_features)
+        for cluster in self._model.get_clusters():
+            df_points = pd.DataFrame(cluster.points, columns=self.X_features)
             df_merged = pd.merge(df, df_points)
             self.clusters.append(df_merged)
 
     def predict(self, X):
         predictions = []
+        X = X.to_numpy()
 
         for x in X:
             # Get the cluster that x belongs to
-            cluster_index = self.model.predict([x])[0]
+            cluster_index = self._model.predict([x])[0]
 
             # Get the points in that cluster
             cluster_points = self.clusters[cluster_index]
@@ -62,9 +77,19 @@ class UnsupervisedClassifier():
             if len(cluster_points) == 0:
                 predictions.append(None)
             else:
-                # Get the y's from those points and get the mode
-                mode = cluster_points[[self.y_feature]
-                                      ].mode().iloc[0].values[0]
+                # Get the y's from those points and get the first mode
+                mode = cluster_points[self.y_feature
+                                      ].mode()[0]
                 predictions.append(mode)
 
         return predictions
+
+    def score(self, X, y):
+        predictions = self.predict(X)
+        y = y.to_numpy()
+        correct = 0
+
+        for i in range(len(predictions)):
+            if predictions[i] == y[i]:
+                correct += 1
+        return correct / len(predictions)
