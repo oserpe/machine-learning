38d61d1840d898d07bdc1ade917a0563c352e477
Gonzalo Arca <goarca@itba.edu.ar>
2022-11-22 18:40:30 -0300

    :fire: remove univariate analysis from ej2 script

--- ej2/main.py
+++ ej2/main.py
@@ -11,11 +11,6 @@ from ..models.unsupervised_classifier import UnsupervisedClassifier
 from ..utils.plots import plot_n_k_fold_cv_eval, plot_cf_matrix, plot_kohonen_matrix_predictions, plot_curves_with_legend
 from ..data.generate_dataset import generate_dataset
 
-def variables_plot(data_df):
-    data_df.hist(edgecolor='black', linewidth=1.0,
-                 xlabelsize=10, ylabelsize=10, grid=False)
-    plt.tight_layout()
-    plt.show()
 
 def elbow_method(X, Ks, times, initial_random_state):
     Ws = []
@@ -31,8 +26,10 @@ def elbow_method(X, Ks, times, initial_random_state):
             Ws_k.append(k_means.variations[-1])
 
         Ws.append(Ws_k)
-    
-    plot_curves_with_legend(Ks, Ws, legends = list(range(times)), X_label = "K", Y_label = "W")
+
+    plot_curves_with_legend(Ks, Ws, legends=list(
+        range(times)), X_label="K", Y_label="W")
+
 
 def plot_kohonen_clustering(movies_df):
     kohonen = Kohonen(max_iter=100, random_state=random_state,
@@ -48,27 +45,32 @@ def plot_kohonen_clustering(movies_df):
         row_index = index // cols
         col_index = index % cols
         feature_weights = kohonen.get_feature_weights(index)
-        sns.heatmap(feature_weights, cmap="YlGnBu", xticklabels=False, yticklabels=False, ax=axes[row_index, col_index])
+        sns.heatmap(feature_weights, cmap="YlGnBu", xticklabels=False,
+                    yticklabels=False, ax=axes[row_index, col_index])
         axes[row_index, col_index].set_title(feature)
     plt.show()
 
     # Plot the number of elements per cluster
     cluster_matrix = kohonen.clusters_to_matrix()
     # Plot heatmap without x and y ticks
-    sns.heatmap(cluster_matrix, annot=True, cmap="YlGnBu", linewidths=0.5, xticklabels=False, yticklabels=False)
+    sns.heatmap(cluster_matrix, annot=True, cmap="YlGnBu",
+                linewidths=0.5, xticklabels=False, yticklabels=False)
     plt.show()
 
     # Plot U-Matrix
     u_matrix = kohonen.get_u_matrix()
     # Plot heatmap
-    sns.heatmap(u_matrix, cmap="Greys_r", annot=True, fmt=".2f", linewidths=0.5, xticklabels=False, yticklabels=False)
+    sns.heatmap(u_matrix, cmap="Greys_r", annot=True, fmt=".2f",
+                linewidths=0.5, xticklabels=False, yticklabels=False)
     plt.show()
+
 
 def kohonen_matrix_predictions(movies_df, only_genres_df):
     classes = only_genres_df.unique().tolist()
     X_features = movies_df.columns.tolist()
     y_feature = only_genres_df.name
-    unsupervised_classifier = UnsupervisedClassifier("kohonen", K=5, max_iter=100)
+    unsupervised_classifier = UnsupervisedClassifier(
+        "kohonen", K=5, max_iter=100)
 
     test_samples = 250
     unsupervised_classifier.fit(
@@ -79,7 +81,9 @@ def kohonen_matrix_predictions(movies_df, only_genres_df):
     # Plot kohonen matrix with clusters
     kohonen_predictions = unsupervised_classifier.model.predict(
         movies_df.values)
-    plot_kohonen_matrix_predictions(unsupervised_classifier._model, y, kohonen_predictions, classes)
+    plot_kohonen_matrix_predictions(
+        unsupervised_classifier._model, y, kohonen_predictions, classes)
+
 
 def n_k_fold(model, movies_df, only_genres_df):
     classes = only_genres_df.unique().tolist()
@@ -90,15 +94,14 @@ def n_k_fold(model, movies_df, only_genres_df):
     # Plot N-K-Fold
     n = 5
     k = 5
-    plot_n_k_fold_cv_eval(movies_df.values, only_genres_df.values, n=n, model=unsupervised_classifier, k=k, X_features=X_features, y_feature=y_feature, classes=classes)
-    
+    plot_n_k_fold_cv_eval(movies_df.values, only_genres_df.values, n=n, model=unsupervised_classifier,
+                          k=k, X_features=X_features, y_feature=y_feature, classes=classes)
+
+
 if __name__ == "__main__":
     random_state = 1
 
     movies_df, only_genres_df = generate_dataset()
-
-    # ------ Analisis univariado de variables ------
-    # variables_plot(movies_df)
 
     # ------- PLOTS ------- #
     # Plot metodo del codo para elegir K en K_means
