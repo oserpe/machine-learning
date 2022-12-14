77e865d9651fe7148a559c4598b68da737e49ba9
manurodriguez <manurodriguez@itba.edu.ar>
2022-12-14 01:02:03 -0300

    :sparkles: add accuracy evolution plot

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from .models.adaboost import adaboost_classify
 from .dataset.utils import prepare_dataset
 from .dataset.analysis import dataset_analysis
 from .utils.estimator import Estimator
+from .metrics.metrics import Metrics
 
 result_column = "diagnosis"
 result_column_labels = ["M", "B"]
@@ -36,11 +37,44 @@ def main(estimator: Estimator = Estimator.PERCEPTRON, use_adaboost: bool = False
 
     plot_metrics(y_test, y_pred)
 
+def plot_accuracy_evolution(estimator: Estimator, random_state: int = 1):
+    X, y = prepare_dataset(standardize=True)
+
+    # standardize
+    X = pd.DataFrame(StandardScaler().fit_transform(X), columns=X.columns)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=random_state)
+
+    accuracies_train = []
+    accuracies_test = []
+    estimators_range = [10, 25, 50, 100, 200, 500, 1000, 2500, 5000, 7500, 10000, 15000]
+    for i in estimators_range:
+        print(f"Estimators: {i}")
+        y_train_pred, y_test_pred = adaboost_classify(
+            estimator, X_train, X_test, y_train, random_state, n_estimators=i, with_y_train_predictions=True)
+        cf_train = Metrics.get_confusion_matrix(
+        y_train, y_train_pred, ["M", "B"])
+        cf_test = Metrics.get_confusion_matrix(
+        y_test, y_test_pred, ["M", "B"])
+        accuracies_train.append((Metrics.get_accuracy_for_class(cf_train, "M") + Metrics.get_accuracy_for_class(cf_train, "B")) / 2)
+        accuracies_test.append((Metrics.get_accuracy_for_class(cf_test, "M") + Metrics.get_accuracy_for_class(cf_test, "B")) / 2)
+
+    # plot
+    import matplotlib.pyplot as plt
+    plt.plot(estimators_range, accuracies_train, marker='o', label="Train", color="blue")
+    plt.plot(estimators_range, accuracies_test, marker='o', label="Test", color="red")
+    plt.xlabel("Number of estimators")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
     data_df = pd.read_csv(
         "./machine-learning/dataset/breast_cancer_wisconsin_data.csv", header=0, sep=',')
     # dataset_analysis(data_df)
+    plot_accuracy_evolution(Estimator.DECISION_TREE)
+    exit(1)
     estimator = Estimator.DECISION_TREE
     use_adaboost = True
     random_state = 1
--- models/adaboost.py
+++ models/adaboost.py
@@ -4,13 +4,16 @@ from sklearn.ensemble import AdaBoostClassifier
 from ..utils.estimator import Estimator
 
 
-def adaboost_classify(estimator: Estimator, X_train, X_test, y_train, random_state):
+def adaboost_classify(estimator: Estimator, X_train, X_test, y_train, random_state, n_estimators=100, learning_rate=0.1, with_y_train_predictions = False):
     subjacent_estimator = estimator.value["estimator"]
     subjacent_estimator.random_state = random_state
 
     adaboost = AdaBoostClassifier(estimator=subjacent_estimator, random_state=random_state,
-                                  algorithm=estimator.value["algorithm"], n_estimators=100, learning_rate=0.1)
+                                  algorithm=estimator.value["algorithm"], n_estimators=n_estimators, learning_rate=learning_rate)
 
     adaboost.fit(X_train, y_train)
+
+    if with_y_train_predictions:
+        return adaboost.predict(X_train), adaboost.predict(X_test)
 
     return adaboost.predict(X_test)
